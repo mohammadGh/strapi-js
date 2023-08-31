d0354a06006430ac397b392a54a5e6c4154d44b6
mgh <m.ghoreshi@gmail.com>
2023-08-31 19:05:54 +0330

    package export tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,10 +2,13 @@ import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest'
 import { StrapiContentSdk, StrapiUserSdk } from '../src'
 import { strapiMockServer as server } from './mocks/strapiMockServer'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(typeof StrapiContentSdk).toEqual('function')
+describe('Sdk package exporting', () => {
+  it('should export user-sdk as a function', () => {
     expect(typeof StrapiUserSdk).toEqual('function')
+  })
+
+  it('should export content-sdk as a function', () => {
+    expect(typeof StrapiContentSdk).toEqual('function')
   })
 })
 
