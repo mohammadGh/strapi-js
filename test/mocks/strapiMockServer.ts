e824103211a851689bb50c8cba4026b211d789f6
mgh <m.ghoreshi@gmail.com>
2023-09-02 12:27:47 +0330

    add login-tests

--- test/mocks/strapiMockServer.ts
+++ test/mocks/strapiMockServer.ts
@@ -1,31 +1,37 @@
+/* eslint-disable no-console */
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
+import loginBadRequestResponse from './loginBadRequestResponse.json'
+import loginBadCredentialResponse from './loginBadCredentialResponse.json'
+import loginOkResponse from './loginOkResponse.json'
 
-const loginBadRequestJsonResponse = './loginBadRequestJsonResponse.json'
-const loginBadCredentialsJsonResponse = './loginBadCredentialsJsonResponse.json'
-const loginOk = './loginOk.json'
+const baseUrl = 'http://cms.gazmeh.ir:1667'
 
 export const strapiMockServer = setupServer(
-  rest.post('/api/auth/local', (req: any, res: any, ctx: any) => {
-    const { identifier, password } = req.params
+  rest.post(`${baseUrl}/api/auth/local`, async (req, res, ctx) => {
+    const { identifier, password } = await req.json()
+    console.log(' ==== IDENTIFIER PARMA === : ', identifier, ' , ', password)
 
     // bad request scenario
     if (!identifier) {
+      console.log(' ==== THE REQUEST MOCKED ===> 1')
       return res(
         ctx.status(400),
-        ctx.json(loginBadRequestJsonResponse))
+        ctx.json(loginBadRequestResponse))
     }
 
     // bad credential scenario
-    if (password && password as string !== 'invalid-password') {
+    if (password && password as string === 'invalid-password') {
+      console.log(' ==== THE REQUEST MOCKED ===> 2')
       return res(
         ctx.status(400),
-        ctx.json(loginBadCredentialsJsonResponse))
+        ctx.json(loginBadCredentialResponse))
     }
 
+    console.log(' ==== THE REQUEST MOCKED ===> 3')
     // otherwise: successful login scenario
     return res(
       ctx.status(200),
-      ctx.json(loginOk))
+      ctx.json(loginOkResponse))
   }),
 )
